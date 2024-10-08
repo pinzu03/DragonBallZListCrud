26b2ae06725370029d93434ba56db5cd239c35c9
David Pineda <davidpinedazuluaga@gmail.com>
2024-10-08 13:52:15 -0500

    Construccion de una especie de crud sin conexion backed en la cual se pueden listar los personajes de dragon ball se pueden agregar nuevos personajes y se pueden eliminar personajes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { DbzModule } from './dbz/dbz.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
